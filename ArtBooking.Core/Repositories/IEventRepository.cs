85f88cf835df825bd95c09114615c762c332e387
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-05-24 12:12:54 +0200

    EventRepositoryInMemory z jedną metodą

---
+++ ArtBooking.Core/Repositories/IEventRepository.cs
@@ -0,0 +1,15 @@
+﻿using ArtBooking.Core.Model;
+using ArtBooking.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtBooking.Core.Repositories
+{
+    public interface IEventRepository
+    {
+        Task<IQueryable<Event>> GetMultipleAsync();
+    }
+}
---
+++ ArtBooking.Storage.InMemory/Storage/EventRepositoryInMemory.cs
@@ -0,0 +1,25 @@
+﻿using ArtBooking.Core.Model;
+using ArtBooking.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtBooking.Storage.InMemory.Storage
+{
+    public class EventRepositoryInMemory : IEventRepository
+    {
+        private readonly ArtBookingDbContext _context;
+
+        public EventRepositoryInMemory(ArtBookingDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IQueryable<Event>> GetMultipleAsync()
+        {
+            return await Task.FromResult(_context.Events);
+        }
+    }
+}
