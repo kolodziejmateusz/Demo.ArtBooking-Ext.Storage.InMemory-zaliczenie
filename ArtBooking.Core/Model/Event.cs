54113287552f4c9fbb2cd976a43438a09ed10627
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-05-24 12:03:59 +0200

    Model Event i dodanie go do dbcontext

---
+++ ArtBooking.Core/Model/Event.cs
@@ -0,0 +1,24 @@
+﻿using ArtBooking.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtBooking.Core.Model
+{
+    public class Event : MetaEntity
+    {
+        [Required]
+        public int EventId { get; set; }
+
+        public int OrganizationId { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public string? EventName { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public Address? Address { get; set; }
+    }
+}
--- ArtBooking.Storage.InMemory/Storage/ArtBookingDbContext.cs
+++ ArtBooking.Storage.InMemory/Storage/ArtBookingDbContext.cs
@@ -1,3 +1,4 @@
+using ArtBooking.Core.Model;
 using ArtBooking.Model;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +12,8 @@ public class ArtBookingDbContext : DbContext, IArtBookingStorageContext
     public DbSet<Organization> Organizations { get; set; }
 
     public DbSet<Location> Locations { get; set; }
+
+    public DbSet<Event> Events { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -52,6 +55,23 @@ public class ArtBookingDbContext : DbContext, IArtBookingStorageContext
 
             // Other configurations for Organization
             builder.HasKey(l => l.LocationId);
+        });
+
+        modelBuilder.Entity<Event>(builder =>
+        {
+            // Map Address properties directly inside the Organizations table
+            builder.OwnsOne(o => o.Address, am =>
+            {
+                // Specify the column names if desired, or let EF use default naming conventions
+                am.Property(a => a.Street);
+                am.Property(a => a.Town);
+                am.Property(a => a.AddressNumber);
+                am.Property(a => a.PostalCode);
+                am.Property(a => a.Country);
+            });
+
+            // Other configurations for Organization
+            builder.HasKey(o => o.EventId);
         });
     }
 }
